b5413cf6f7ec5b455c35449fa54ab41d34a628f7
karzai24 <31968092+karzai24@users.noreply.github.com>
2020-09-12 02:04:06 +0100

    Add files via upload

---
+++ deployfargate-cf-template/pushTemplate.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# This will create Cloudformation requested by pushTemplate.yml
+# ansible-playbook -i "./inventories/hosts" -e "Regions=us-east-1" plays/pushTemplate.yml -v
+# Example of launching script: ./pushTemplate.sh us-east-1 
+
+if [[ $# -lt 1 ]] ; then
+    echo -e '\n# You need to provide a Region'
+    echo -e '#\n'
+    echo -e 'Please refer to README.md\n'
+    exit 64 # sysexits "command line usage" error
+fi
+
+
+echo -e "\nVariables used:"
+echo "# Region: $Region"
+
+ansible-playbook -i "./inventories/hosts" -e "Region=$Region"  plays/pushTemplate.yml -v
+RESULT=$?
+echo -e "Return Code: $RESULT\n"
+
+echo -e "Done.\n"
